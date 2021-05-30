53e694321aafea7ce890e82f14944f3e360086ac
bukaj <jakubn2004@gmail.com>
2021-05-30 15:41:29 +0200

    Drugi bo test

--- app/src/main/java/com/mand/lubris/MainActivity.java
+++ app/src/main/java/com/mand/lubris/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private String kok="sdfds";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
